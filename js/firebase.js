d7a1fd1cf18276a05d45a1a6f82d5832d8d65fc5
Jenny Kim <kimsujin128@naver.com>
2021-02-24 23:17:51 +0900

    add reg

--- js/firebase.js
+++ js/firebase.js
@@ -16,20 +16,51 @@ const sendBtn = document.getElementById('sendBtn');
 
 const database = firebase.database();
 
-sendBtn.addEventListener('click',(e) => {
+sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
+  check();
+});
+
+// Input Check
+const emailCheck = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+
+function check() {
+  const nameValue = name.value;
+  const emailValue = email.value;
+  const messageValue = message.value;
+  const form = document.querySelector('.contact__form');
+
+  if (nameValue == '') {
+    alert('이름을 입력해 주세요.');
+    name.focus();
+    return;
+  }
+  if (!emailCheck.test(emailValue)) {
+    alert('email을 다시 입력해 주세요.');
+    email.focus();
+    return;
+  }
+  if (messageValue == '') {
+    alert('메시지를 입력해 주세요.');
+    message.focus();
+    return;
+  }
+
+  // Insert Data
   database.ref('/users/' + name.value).set({
 
     nameV: name.value,
     emailV: email.value,
     messageV: message.value
   });
-  alert('메시지가 성공적으로 보내졌습니다.');
-  resetInput();
-});
 
-function resetInput(){
+  alert('메시지가 성공적으로 전송되었습니다');
+
+  resetInput();
+}
+
+function resetInput() {
   name.value = '';
   email.value = '';
   message.value = '';
